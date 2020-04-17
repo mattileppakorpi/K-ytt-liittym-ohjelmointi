448c969386d627de337a9e69240b668df2ec933b
Matti Leppäkorpi <matti.leppakorpi@gail.com>
2020-04-17 15:55:53 +0300

    uudet tiedostot

---
+++ Leikkipaikat/Leikkipaikat/BL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leikkipaikat
+{
+    public class Playground
+    {
+        public string Address { get; set; }
+        public string Info { get; set; }
+        public List<Equipment> Equipment { get; set; }
+        
+    }
+    
+    public class Equipment
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public List<string> Faults { get; set; }
+
+    }
+
+}
