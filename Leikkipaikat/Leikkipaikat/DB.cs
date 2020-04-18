5df30570bf1d68cce7e248f4cd685dd36f148db4
Matti Leppäkorpi <matti.leppakorpi@gail.com>
2020-04-18 19:57:53 +0300

    DBmukaan

---
+++ Leikkipaikat/Leikkipaikat/DB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Leikkipaikat
+{
+     public class DB
+    {
+        public static DataTable GetPlayGrounds()
+        {
+			try
+			{
+				DataTable dt = new DataTable();
+
+			}
+			catch 
+			{
+
+				throw;
+			}
+
+        }
+    }
+}
