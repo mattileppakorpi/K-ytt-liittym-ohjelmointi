0b59c6ef2821ed18255823a68d872cd512afaf5f
Matti Leppäkorpi <matti.leppakorpi@gail.com>
2020-04-19 20:05:36 +0300

    Eka versio

--- Leikkipaikat/Leikkipaikat/DB.cs
+++ Leikkipaikat/Leikkipaikat/DB.cs
@@ -4,24 +4,235 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using LiteDB;
 
 namespace Leikkipaikat
 {
-     public class DB
+    public static class DB
     {
-        public static DataTable GetPlayGrounds()
+        public static List<Playground> GetPlaygrounds()
         {
-			try
-			{
-				DataTable dt = new DataTable();
+            //Haetaan tietokannasta lista kohteista.
+            List<Playground> playgrounds = new List<Playground>();
+            try
+            {
+                using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+                {
+                    var col = db.GetCollection<Playground>("playgrounds");
+                    playgrounds = (List<Playground>)col;
+                }
+                return playgrounds;
+            }
+            catch (Exception)
+            {
 
-			}
-			catch 
-			{
-
-				throw;
-			}
-
+                throw;
+            }
         }
+        public static bool AddPlayground(string address, string info)
+        {
+            //Lisätään tietokantaan kohde.
+            try
+            {
+
+                var playground = new Playground{Address = address,Info = info};
+                using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+                {
+                    var col = db.GetCollection<Playground>("playgrounds");
+                    col.Insert(playground);
+                }
+
+
+                return true;
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static bool DeletePlayground(string playground)
+        {
+            //Poistetaan tietokannasta kohde.
+            try
+            {
+                using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+                {
+                    
+                    var col = db.GetCollection<Playground>("playgrounds");
+                    var p = col.EnsureIndex(x => x.Address);
+                    if(playground.Equals(p) == true)
+                    col.Delete(p);
+                };
+                return true;
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static bool UpdatePlayground(Playground playground, string address, string info)
+        {
+            //Muokataan kohteen tietoja ja tallennetaan.
+            try
+            {
+                using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+                {
+                    var col = db.GetCollection<Playground>("playgrounds");
+                    List<Playground> playgrounds = new List<Playground>();
+                    playgrounds = (List<Playground>)col;
+                    foreach (var item in playgrounds)
+                    {
+                        if (playground.Address.Equals(item.Address))
+                           item.Address = address;
+                        item.Info = info;
+                        col.Update(item);
+
+                    }
+                    
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static bool AddEquipment(Playground playground, Equipment equipment)
+        {
+            //Lisätään valittuun kohteeseen tietokantaan väline.
+            try
+            {
+
+               
+                //playground.Equipment.Add(equipment);
+                using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+                {
+                    var col = db.GetCollection<Playground>("playgrounds");
+                    List<Playground> playgrounds = new List<Playground>();
+                    playgrounds = (List<Playground>)col;
+                    foreach (var item in playgrounds)
+                    {
+                        if (playground.Address.Equals(item.Address))
+                            item.Equipment.Add(equipment);
+                        col.Update(item);
+
+                    }
+
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static bool DelEquipment(Playground playground, Equipment equipment)
+        {
+            //Poistetaan valitusta kohteesta väline tietokannasta.
+            try
+            {
+                //playground.Equipment.Remove(equipment);
+                using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+                {
+                    var col = db.GetCollection<Playground>("playgrounds");
+                    List<Playground> playgrounds = new List<Playground>();
+                    playgrounds = (List<Playground>)col;
+                    foreach (var item in playgrounds)
+                    {
+                        if (playground.Address.Equals(item.Address))
+                            item.Equipment.Remove(equipment);
+                        col.Update(item);
+
+                    }
+
+                }
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static bool AddFault(Playground playground, Equipment equipment, string fault)
+        {
+            //Lisätään tietyn kohteen tiettyyn välineeseen vika
+            try
+            {
+                //equipment.Faults.Add(fault);
+                using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+                {
+                    var col = db.GetCollection<Playground>("playgrounds");
+                    List<Playground> playgrounds = new List<Playground>();
+                    playgrounds = (List<Playground>)col;
+                    foreach (var item in playgrounds)
+                    {
+                        if (playground.Address.Equals(item.Address))
+                            
+                            foreach (var item2 in item.Equipment)
+                            {
+                                if (item2.Name.Equals(item2.Name))
+                                    item2.Faults.Add(fault);
+                            }
+                        col.Update(item);
+
+                    }
+
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static bool DelFault(Playground playground, Equipment equipment, string fault)
+        {
+            //Poistetaan tietyn kohteen tietystä välineestä vika
+            try
+            {
+                //equipment.Faults.Remove(fault);
+                using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+                {
+                    var col = db.GetCollection<Playground>("playgrounds");
+                    List<Playground> playgrounds = new List<Playground>();
+                    playgrounds = (List<Playground>)col;
+                    foreach (var item in playgrounds)
+                    {
+                        if (playground.Address.Equals(item.Address))
+
+                            foreach (var item2 in item.Equipment)
+                            {
+                                if (item2.Name.Equals(item2.Name))
+                                    item2.Faults.Remove(fault);
+                            }
+                        col.Update(item);
+
+                    }
+
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+
+
     }
 }
--- Leikkipaikat/Leikkipaikat/MainWindow.xaml.cs
+++ Leikkipaikat/Leikkipaikat/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace Leikkipaikat
 {
@@ -20,15 +21,19 @@ namespace Leikkipaikat
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static string originalAddress = "";
         public MainWindow()
         {
             InitializeComponent();
         }
 
+
         private void btnGetPlaygrounds_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                //Haetaan hakunapilla leikkipaikat dgPlaygrounds-datagridiin tietokannasta
+                dgPlaygrounds.ItemsSource = Leikkipaikat.DB.GetPlaygrounds();
 
             }
             catch (Exception ex)
@@ -37,6 +42,161 @@ namespace Leikkipaikat
                 MessageBox.Show("Ohjelman suorituksen aikana tapahtui seuraava virhe: " + ex.Message);
             }
 
+        }
+
+        private void btnAddPlaygrounds_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Lisätään uusi kohde, tiedot tekstikentistä osoite ja info. Toisella rivillä päivitetään datagridi.
+                Leikkipaikat.DB.AddPlayground(txtAddress.Text, txtInfo.Text);
+                dgPlaygrounds.ItemsSource = Leikkipaikat.DB.GetPlaygrounds();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private void btnDelPlayground_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Poistetaan datagridistä valittu kohde. Viimeisellä rivillä taas päivitetään.
+                string chosen = dgPlaygrounds.SelectedItem.ToString();
+                Leikkipaikat.DB.DeletePlayground(chosen);
+
+                dgPlaygrounds.ItemsSource = Leikkipaikat.DB.GetPlaygrounds();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private void btnModify_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                //Muokataan valittua kohdetta, tiedot osoite ja info kentistä, lopussa päivitetään.
+                Playground selected = (Playground)dgPlaygrounds.SelectedItem;
+                string address = txtAddress.Text;
+                string info = txtInfo.Text;
+                Leikkipaikat.DB.UpdatePlayground(selected, address, info);
+
+                dgPlaygrounds.ItemsSource = Leikkipaikat.DB.GetPlaygrounds();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void dgPlaygrounds_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Valitun kohteen tiedot menevät osoite- ja info-kenttiin. Näin kohdetta voi muokata halutessa.
+            int i = dgPlaygrounds.SelectedIndex;
+            if (i > -1)
+            {
+                var dt = (DataTable)dgPlaygrounds.DataContext;
+                DataRow dr = dt.Rows[i];
+                originalAddress = dr[0].ToString();
+                txtAddress.Text = dr[0].ToString();
+                txtInfo.Text = dr[1].ToString();
+
+                //Valitun kohteen info näytetään dgInfo-datagridissä ja välineet gdEquipment-datagridissä.
+                Playground playground = (Playground)dgPlaygrounds.SelectedItem;
+                dgEquipment.DataContext = playground.Equipment;
+                dgInfo.DataContext = playground.Info;
+            }
+        }
+        private void dgEquipment_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //dgequipment-kentän valitun välineen viat menevät dgFaults-datagridiin.
+            Equipment equipment = (Equipment)dgEquipment.SelectedItem;
+            dgFaults.DataContext = equipment.Faults;
+        }
+
+        private void btnAddEquipment_Click(object sender, RoutedEventArgs e)
+        {
+            //Lisätään valitulle kohteelle väline, tiedot txtequipment ja txtbrand-kentistä.
+            try
+            {
+                Equipment equipment = new Equipment();
+                equipment.Name = txtEquipment.Text;
+                equipment.Brand = txtBrand.Text;
+                Playground selected = (Playground)dgPlaygrounds.SelectedItem;
+                Leikkipaikat.DB.AddEquipment(selected, equipment);
+
+               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private void btnDelEquipment_Click(object sender, RoutedEventArgs e)
+        {
+            //Poistetaan valitun kohteen valittu väline.
+            try
+            {
+                Playground selected = (Playground)dgPlaygrounds.SelectedItem;
+                Equipment equipment = (Equipment)dgEquipment.SelectedItem;
+
+                Leikkipaikat.DB.DelEquipment(selected, equipment);
+
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnAddFault_Click(object sender, RoutedEventArgs e)
+        {
+            //Lisätään vika valitun kohteen valittuun välineeseen. Vika txtFault-kentästä.
+            try
+            {
+                Playground selected = (Playground)dgPlaygrounds.SelectedItem;
+                Equipment equipment = (Equipment)dgEquipment.SelectedItem;
+                string fault = txtFault.Text;
+                Leikkipaikat.DB.AddFault(selected, equipment, fault);
+
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnDelFault_Click(object sender, RoutedEventArgs e)
+        {
+            //Poistetaan valittu vika valitun kohteen valitusta välineestä.
+            try
+            {
+                Playground selected = (Playground)dgPlaygrounds.SelectedItem;
+                Equipment equipment = (Equipment)dgEquipment.SelectedItem;
+                string fault = dgFaults.SelectedItem.ToString();
+                Leikkipaikat.DB.DelFault(selected, equipment, fault);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
